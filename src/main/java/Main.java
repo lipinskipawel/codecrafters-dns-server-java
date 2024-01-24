a82f84194718e73784e49375da89e5235588d6d0
Pawel Lipinski <lipinskijob@gmail.com>
2024-01-24 07:17:32 +0100

    Write header section

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.util.BitSet;
+
+import static java.nio.ByteOrder.BIG_ENDIAN;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.out.println("Logs from your program will appear here!");
-
         try (DatagramSocket serverSocket = new DatagramSocket(2053)) {
             while (true) {
                 final byte[] buf = new byte[512];
@@ -14,7 +15,21 @@ public class Main {
                 serverSocket.receive(packet);
                 System.out.println("Received data");
 
-                final byte[] bufResponse = new byte[512];
+                short id = (short) 1234;
+                final var bitSet = new BitSet(8);
+                bitSet.flip(7);
+                short zero = (short) 0;
+                final var bufResponse = ByteBuffer.allocate(12)
+                        .order(BIG_ENDIAN)
+                        .putShort(id)
+                        .put(bitSet.toByteArray())
+                        .put((byte) 0)
+                        .putShort(zero)
+                        .putShort(zero)
+                        .putShort(zero)
+                        .putShort(zero)
+                        .array();
+
                 final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
                 serverSocket.send(packetResponse);
             }
